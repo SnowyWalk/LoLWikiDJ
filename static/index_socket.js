b8f022dcac77ba92143eeb3f470f5689db44326e
SnowyWalk <chunsa1166jp@gmail.com>
2021-09-19 04:23:03 +0900

    후원순위 변동

--- static/index_socket.js
+++ static/index_socket.js
@@ -114,13 +114,16 @@ socket.on('login', function(isSuccess) {
 	add_system_message('후원 랭킹\n'
 						+ '★ 0. Lily(샤르프로젝트) 님 ★\n' 
 						+ '1. 우뭇가사리 님\n' 
-						+ '2. 랠래 님\n'
-						+ '3. 노통 님\n'
-						+ '4. 고냥이지 님\n'
-						+ '5. 인공사 님\n'
-						+ '6. pagolas 님\n'
-						+ '7. 디아 님\n'
-						+ '8. 강령군주 님\n'
+						+ '2. 노통 님\n'
+						+ '3. 다정이 님\n'
+						+ '4. 콘파고 님\n'
+						+ '5. 랠래 님\n'
+						+ '6. 고냥이지 님\n'
+						+ '7. pagolas 님\n'
+						+ '8. 디아 님\n'
+						+ '9. 인공사 님\n'
+						+ '10. 강령군주 님\n'
+						+ '11. 돌고래대통령 님\n'
 						+ '후원 계좌 : 기업 539-028793-01-012 박*준', 'yellow')
 	add_system_message('명령어 목록은 /? 을 입력해 볼 수 있습니다.')
 	var patchnote = add_system_message('패치 노트 보기 (클릭)', 'chartreuse')
