0daee19e2bbd2336ee470a754d55a52f992c79e5
SnowyWalk <chunsa1166jp@gmail.com>
2022-07-10 18:44:37 +0900

    로그인 창에서 노래 재생 시 들리던 버그 수정

--- static/index_socket.js
+++ static/index_socket.js
@@ -171,6 +171,10 @@ socket.on('djs', function(data_list) {
 
 /* 서버로부터 영상 데이터를 받은 경우 */
 socket.on('update_current_video', function(data) {
+
+	if(!g_isLogin)
+		return
+
 	if(data)
 	{
 		if(data.video_id && data.video_id != g_current_video_id)
